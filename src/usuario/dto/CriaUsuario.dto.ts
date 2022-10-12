dfe0245434a729dd9045c6a36c9b9e25fe6a3345
Wanderson M <wandersonmaceds@gmail.com>
2022-10-12 14:42:35 -0300

    validando usuário

---
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CriaUsuarioDTO {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  nome: string;
+
+  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
+  email: string;
+
+  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+  senha: string;
+}
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
@@ -6,7 +7,7 @@ export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criaUsuario(@Body() dadosDoUsuario) {
+  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
     this.usuarioRepository.salvar(dadosDoUsuario);
     return dadosDoUsuario;
   }
