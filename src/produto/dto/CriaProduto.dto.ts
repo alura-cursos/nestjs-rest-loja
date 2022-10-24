be55ee568662623b64e39cd9f077f95043e0801a
Wanderson M <wandersonmaceds@gmail.com>
2022-10-24 20:03:38 -0300

    relacionando produto e usuário

--- src/produto/dto/CriaProduto.dto.ts
+++ src/produto/dto/CriaProduto.dto.ts
@@ -6,6 +6,7 @@ import {
   IsNumber,
   IsString,
   IsUrl,
+  IsUUID,
   MaxLength,
   Min,
   ValidateNested,
@@ -31,6 +32,9 @@ export class ImagemProdutoDTO {
 }
 
 export class CriaProdutoDTO {
+  @IsUUID(undefined, { message: 'ID de usuário inválido' })
+  usuarioId: string;
+
   @IsString()
   @IsNotEmpty({ message: 'Nome do produto não pode ser vazio' })
   nome: string;
